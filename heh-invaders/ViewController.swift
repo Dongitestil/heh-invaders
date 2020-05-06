b985b2c9e8589a94335ab7250568d08689cf92ff
Dongitestil <golgfermo@gmail.com>
2020-05-06 22:38:02 +0700

    Initial Commit

---
+++ heh-invaders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  heh-invaders
+//
+//  Created by student on 06/05/2020.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
