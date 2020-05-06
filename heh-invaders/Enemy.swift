663d8eff34da5be1a456cd32c4e29eafedc5b6ba
Dongitestil <golgfermo@gmail.com>
2020-05-07 01:37:05 +0700

    Added entities and their move logic.

---
+++ heh-invaders/Enemy.swift
@@ -0,0 +1,15 @@
+//
+//  Enemy.swift
+//  heh-invaders
+//
+//  Created by student on 07/05/2020.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Enemy: UIImageView {
+    //let hp: Int = 1
+    //let cost: Int = 1
+}
--- heh-invaders/ViewController.swift
+++ heh-invaders/ViewController.swift
@@ -12,16 +12,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ship: UIImageView!
     
+    var enemies = [[Enemy]]()
+    var cdEnemy = 10
+    
+    var left = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        spawnEnemies()
+        
+        startTimer()
         // Do any additional setup after loading the view.
+    }
+    
+    func spawnEnemies() {
+        for i in 0...3 {
+            var enemiesRow = [Enemy]()
+            for j in 0...2 {
+                let frame = CGRect(x: view.frame.width * 0.2 * CGFloat(i) + view.frame.width * 0.1,
+                                   y: view.frame.height * 0.05 + view.frame.width * 0.2 * CGFloat(j),
+                                   width: view.frame.width * 0.2,
+                                   height: view.frame.width * 0.2)
+                let newEnemy = Enemy(frame: frame)
+                newEnemy.image = #imageLiteral(resourceName: "enemy")
+                view.addSubview(newEnemy)
+                enemiesRow.append(newEnemy)
+            }
+            enemies.append(enemiesRow)
+        }
     }
     
     @objc func startTimer() {
         //if !isGameOver {
         let gameLoopStart = mach_absolute_time()
             
-        //do something
+        changeEnemyPosition()
             
         let gameLoopEnd = mach_absolute_time()
         let dTime = Double(gameLoopStart/1000000000) + Double(1.0/60.0) - Double(gameLoopEnd/1000000000)
@@ -33,7 +58,44 @@ class ViewController: UIViewController {
         //}
     }
 
-
+    func changeEnemyPosition() {
+        cdEnemy += 1
+        
+        if cdEnemy >= 10 {
+            for i in 0...3 {
+                for j in 0...2 {
+                    let enemy = enemies[i][j]
+                    if left {
+                        enemy.frame.origin.x -= view.frame.width * 0.01
+                    } else {
+                        enemy.frame.origin.x += view.frame.width * 0.01
+                    }
+                }
+            }
+            if enemies[0][0].frame.origin.x - view.frame.width * 0.01 <= 0 {
+                left = false
+            }
+            if enemies[3][2].frame.origin.x + enemies[3][2].frame.width + view.frame.width * 0.01 >= view.frame.width {
+                left = true
+            }
+            cdEnemy = 0
+        }
+    }
+    
+    @IBAction func buttonLeft(_ sender: UIButton) {
+        changeShipPosition(direction: -1)
+    }
+ 
+    @IBAction func buttonRight(_ sender: UIButton) {
+        changeShipPosition(direction: 1)
+    }
+    
+    func changeShipPosition(direction: Int) {
+        let dX = view.frame.width * 0.07 * CGFloat(direction)
+        if (ship.frame.origin.x + dX > CGFloat(10)) && (ship.frame.origin.x + dX < view.frame.width - ship.frame.width) {
+            ship.frame.origin.x += dX
+        }
+    }
 
 }
 
